ba704b48e943c4d6010b06a8e0d5649b3bbd1d61
Ashby McFall <ashbymcfall@hotmail.co.uk>
2021-06-08 17:04:13 +0900

    ✨add nav bar item

---
+++ src/components/nav-bar-item/nav-bar-item.component.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './nav-bar-item.styles.scss';
+
+const NavBarItem = (props) => (
+    <div className="nav-bar-item">
+        <p>{this.props.children}</p>
+    </div>
+);
+
+export default NavBarItem;
