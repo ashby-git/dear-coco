4142e55140f0c0bff143d89656a978a605a70784
Ashby McFall <ashbymcfall@hotmail.co.uk>
2021-06-11 17:44:20 +0900

    🐛fix nav bar item hover state to only on text

--- src/components/nav-bar/nav-bar-item/nav-bar-item.component.jsx
+++ src/components/nav-bar/nav-bar-item/nav-bar-item.component.jsx
@@ -3,8 +3,10 @@ import React from 'react';
 import './nav-bar-item.styles.scss';
 
 const NavBarItem = (props) => (
-    <div className="nav-bar-item">
-        <p>{this.props.children}</p>
+    <div className="container">
+        <div className="nav-bar-item">
+            <p>{props.children}</p>
+        </div>
     </div>
 );
 
