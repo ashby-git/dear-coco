117db16170eaa56481f0e45e053e6a35312c2083
Ashby McFall <ashbymcfall@hotmail.co.uk>
2021-06-17 17:47:42 +0900

    💄add online shop button

---
+++ src/components/nav-bar/online-shop-button/online-shop-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import './online-shop-button.styles.scss';
+
+const OnlineShopButton = () => (
+
+    <div className="online-shop-button">
+        {/* <img src="../../../assets/sp_shopping_icon.svg" alt="icon" /> */}
+
+
+
+
+
+
+        <div className="icon-text-container">
+
+            <div className="icon" />
+
+
+
+            <p className="text">
+                Online Shop
+            </p>
+        </div>
+    </div>
+
+);
+
+export default OnlineShopButton;
