f88581cb7dd5b35b734cfe4a4053cbe49dc58f2d
Ashby McFall <ashbymcfall@hotmail.co.uk>
2021-06-18 20:10:29 +0900

    delete white space and dead code

--- src/components/nav-bar/online-shop-button/online-shop-button.component.jsx
+++ src/components/nav-bar/online-shop-button/online-shop-button.component.jsx
@@ -5,19 +5,8 @@ import './online-shop-button.styles.scss';
 const OnlineShopButton = () => (
 
     <div className="online-shop-button">
-        {/* <img src="../../../assets/sp_shopping_icon.svg" alt="icon" /> */}
-
-
-
-
-
-
         <div className="icon-text-container">
-
             <div className="icon" />
-
-
-
             <p className="text">
                 Online Shop
             </p>
