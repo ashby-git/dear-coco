c0d03dcf4e4cfa1a64e570b5f0cbc237df403fcc
Ashby McFall <ashbymcfall@hotmail.co.uk>
2021-06-28 16:28:03 +0900

    WIP - top page content & font styling

---
+++ src/components/top-page/top-page.component.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import './top-page.styles.scss';
+
+const TopPage = () => (
+
+    <div>
+
+
+        <div className="japanese-text-container">
+            <p className="japanese-text">
+                地球に優しいコスメは、
+                <br /><br />
+                人の肌にも優しい
+            </p>
+        </div>
+
+        <div className="english-text-container ">
+            <p className="english-text">
+                {"The power of "}
+                <span className="english-text-big">
+                    COCONUT
+                </span>
+            </p>
+        </div>
+
+
+    </div >
+);
+
+export default TopPage;
