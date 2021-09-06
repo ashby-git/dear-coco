71dbeb044f2529a37ca4826bead9862ebd35569a
Ashby McFall <ashbymcfall@hotmail.co.uk>
2021-09-06 11:50:08 +0900

    WIP - quality-content-group

--- src/components/quality-page/quality-content-group.component.jsx
+++ src/components/quality-page/quality-content-group.component.jsx
@@ -5,18 +5,12 @@ import './quality-content-group.styles.scss';
 const QualityContentGroup = (props) => (
     <div className="container">
         <div className="top-section">
-
             <div className="number">{props.number}</div>
             <div className="subheading">{props.subheading}</div>
-
         </div>
-        <div className="line"></div>
+        <div className="line" />
         <div className="textbox">{props.textbox}</div>
-
     </div>
 );
-
-//<p>{props.children}</p>
-
 
 export default QualityContentGroup;
