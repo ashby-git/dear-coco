2615fcec2a2371f83814a9fa64a63aca65332b47
Ashby McFall <ashbymcfall@hotmail.co.uk>
2021-09-07 12:32:34 +0900

    ♻️Fixed requested changes

--- src/components/concept-page/concept-page.component.jsx
+++ src/components/concept-page/concept-page.component.jsx
@@ -12,51 +12,39 @@ const ConceptPage = () => (
             <div className="title">DEAR COCO is...</div>
             <div className="subheading">
                 オーガニックココナッツから生まれた
-                <br />
-                <br />
                 地球と人に優しいコスメブランドです
             </div>
 
             <div className="two-halfs-container">
-
                 <div className="left-half-container">
+                    <div className="textbox-container">
+                        <div className="textbox1">
+                            生命の木とも呼ばれるココヤシの木。
+                            <br />
+                            その実から採れるココナッツオイルは栄養豊富で、
+                            <br />
+                            美容と健康に多くの利益をもたらします。
+                            <br />
+                            DEAR COCOは、100%オーガニック栽培の
+                            <br />
+                            タイ産ココナッツのみを厳選。
+                        </div>
 
-                    <div className="textbox">
-                        生命の木とも呼ばれるココヤシの木。
-                        <br />
-                        <br />
-                        その実から採れるココナッツオイルは栄養豊富で、
-                        <br />
-                        <br />
-                        美容と健康に多くの利益をもたらします。
-                        <br />
-                        <br />
-                        DEAR COCOは、100%オーガニック栽培の
-                        <br />
-                        <br />
-                        タイ産ココナッツのみを厳選。
-                        <br />
-                        <br />
-                        <br />
-                        <br />
-                        こだわりのココナッツオイルと
-                        <br />
-                        <br />
-                        自然由来の成分のみを使用した
-                        <br />
-                        <br />
-                        地球と人に優しいコスメブランドです。
+                        <div className="textbox2">
+                            こだわりのココナッツオイルと
+                            <br />
+                            自然由来の成分のみを使用した
+                            <br />
+                            地球と人に優しいコスメブランドです。
+                        </div>
                     </div>
+
                     <div className="button-container">
                         <div className="temp-button" />
                     </div>
-
                 </div>
-                {/* <div className="image-container"> */}
                 <img className="image" src={image} alt="img" />
-                {/* </div> */}
             </div>
-
         </div>
     </div>
 );
