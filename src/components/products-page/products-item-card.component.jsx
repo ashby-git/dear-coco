8e4b9cd1cda0ad0d915b78a43e842744e6433e43
Ashby McFall <ashbymcfall@hotmail.co.uk>
2021-09-13 11:07:58 +0900

    💄Make requested changes

--- src/components/products-page/products-item-card.component.jsx
+++ src/components/products-page/products-item-card.component.jsx
@@ -2,21 +2,19 @@ import React from 'react';
 
 import './products-item-card.styles.scss';
 
-// import StandardButton from 
+// TODO import StandardButton from 
 
 const ProductsItemCard = (props) => (
     <div className="card-background">
         <div className="items-container">
-
             <div className="card-heading">
                 ココナッツオイル in<br />
                 {props.cardHeading}
             </div>
-
-            <div className="image">{props.image}</div>
+            <img className="image" src={props.image} alt="img" />
             <div className="textbox">{props.textbox}</div>
             <div className="button-container">
-                {/* < className="standard-button"StandardButton>商品ページへ</StandardButton*/}
+                {/* TODO < className="standard-button"StandardButton>商品ページへ</StandardButton*/}
                 <div className="tempo-standard-button"></div>
             </div>
         </div>
