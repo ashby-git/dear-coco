1c402513f216cdd497e81b66b3a74e06cbb537f4
Ashby McFall <ashbymcfall@hotmail.co.uk>
2021-10-04 09:35:59 +0900

    WIP - news page

---
+++ src/components/news-page/news-page.component.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import './news-page.styles.scss';
+
+// TODO import NewsContentGroup from 
+// TODO import AnimatedButton from 
+
+const NewsPage = (props) => (
+    <div className="news-page-background">
+        <div className="news-title">NEWS</div>
+        <div className="news-subheading">お知らせ</div>
+        <div className="news-content-group-container">
+            <div className="news-content-group"></div>
+            <div className="news-content-group"></div>
+            <div className="news-content-group"></div>
+            <div className="news-content-group"></div>
+            <div className="news-content-group"></div>
+            <div className="news-content-group"></div>
+        </div>
+        <div className="tempo-anim-button"></div>
+
+
+
+
+    </div>
+);
+
+export default NewsPage;
