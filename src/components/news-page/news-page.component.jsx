251bb0d35f949a1cc4fff6228b6144965939d69f
Ashby McFall <ashbymcfall@hotmail.co.uk>
2021-10-04 12:27:19 +0900

    WIP - news page

--- src/components/news-page/news-page.component.jsx
+++ src/components/news-page/news-page.component.jsx
@@ -5,22 +5,26 @@ import './news-page.styles.scss';
 // TODO import NewsContentGroup from 
 // TODO import AnimatedButton from 
 
+// TODO add background leaf
+
 const NewsPage = (props) => (
     <div className="news-page-background">
         <div className="news-title">NEWS</div>
         <div className="news-subheading">お知らせ</div>
-        <div className="news-content-group-container">
-            <div className="news-content-group"></div>
-            <div className="news-content-group"></div>
-            <div className="news-content-group"></div>
-            <div className="news-content-group"></div>
-            <div className="news-content-group"></div>
-            <div className="news-content-group"></div>
+
+        <div className="container-container">
+            <div className="news-content-group-container">
+                <div className="news-content-group"></div>
+                <div className="news-content-group"></div>
+                <div className="news-content-group"></div>
+                <div className="news-content-group"></div>
+                <div className="news-content-group"></div>
+                <div className="news-content-group"></div>
+            </div>
         </div>
-        <div className="tempo-anim-button"></div>
-
-
-
+        <div className="tempo-button-container">
+            <div className="tempo-anim-button"></div>
+        </div>
 
     </div>
 );
