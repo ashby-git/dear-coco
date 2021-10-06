af35dc5e68f7a895726129728ae2e483e3c5b342
Ashby McFall <ashbymcfall@hotmail.co.uk>
2021-10-06 14:31:19 +0900

    💄position elements and change text colours

--- src/components/news-page/news-content-group.component.jsx
+++ src/components/news-page/news-content-group.component.jsx
@@ -4,13 +4,20 @@ import './news-content-group.styles.scss';
 
 const NewsContentGroup = (props) => (
     <div className="news-content-group-background">
-        <img className="image" src={props.image} alt="img" />
-        <div className="text-group-container">
-            <div className="article">{props.article}</div>
-            <div className="date">{props.date}</div>
-            <div className="link">{props.link}</div>
+        <div className="two-sides-container">
+            <div className="image-container">
+                <img className="image" src={props.image} alt="img" />
+            </div>
+            <div className="text-group-container">
+                <div className="text-group">
+                    <div className="article">{props.article}</div>
+                    <div className="bottom-text-group">
+                        <div className="date">{props.date}</div>
+                        <div className="link">{props.link}</div>
+                    </div>
+                </div>
+            </div>
         </div>
-
     </div>
 );
 
