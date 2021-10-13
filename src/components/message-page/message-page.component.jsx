085c72eaa2d2786e574b745dcd1ae85370ce1655
Ashby McFall <ashbymcfall@hotmail.co.uk>
2021-10-13 18:40:07 +0900

    🚧add text from design, import bg img

--- src/components/message-page/message-page.component.jsx
+++ src/components/message-page/message-page.component.jsx
@@ -2,18 +2,22 @@ import React from 'react';
 
 import './message-page.styles.scss';
 
-const MessagePage = (props) => (
+const MessagePage = () => (
 
     <div className="message-container">
         <div className="message-background">
             <div className="message-card">
-                <div className="message-title"></div>
+                <div className="message-title">地球の恵み、ココナッツへの 愛を込めて。</div>
                 <div className="message-text-group">
-                    <div className="paragraph"></div>
-                    <div className="paragraph"></div>
-                    <div className="paragraph"></div>
+                    <p className="paragraph">豊富な栄養を持つことで知られるココナッツは、多くの国で食材として用いられるほか、その殻や繊維までも余すことなく資源として利用されています。 熱帯地方の人々の暮らしを豊かにするココナッツは、まさに地球からのギフト。</p>
+                    <p className="paragraph">私達DEAR COCOはこの地球の恵みに感謝し、ココナッツの持つパワーを多くの皆様にお届けできるよう、より良い製品作りに努めてまいります。</p>
+                    <p className="paragraph">製品に関するご意見、ご質問は、下記よりお問い合わせください。</p>
                 </div>
-                <div className="tempo-ani-button"></div>
+                <div className="message-button-container">
+
+                    {/* お問い合わせページへ */}
+
+                </div>
             </div>
         </div>
     </div>
