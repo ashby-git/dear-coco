b7d039928666edbeebfd8ed5f1994f356bffd1a8
Ashby McFall <ashbymcfall@hotmail.co.uk>
2021-10-26 12:09:57 +0900

    ✨import ProductsItemCard and AnimatedButton

--- src/components/products-page/products-page.component.jsx
+++ src/components/products-page/products-page.component.jsx
@@ -2,41 +2,39 @@ import React from 'react';
 
 import './products-page.styles.scss';
 
-// import ProductsItemCard
-// import AnimatedButton
+import ProductsItemCard from '../products-page/products-item-card.component';
+import AnimatedButton from '../animated-button/animated-button.component';
 
-// import productsImage1 from '../../assets/products_img_01.jpg'
-// import productsImage2 from '../../assets/products_img_02.jpg'
-// import productsImage3 from '../../assets/products_img_01.jpg'
+import productsImage1 from '../../assets/products_img_01.jpg'
+import productsImage2 from '../../assets/products_img_02.jpg'
+import productsImage3 from '../../assets/products_img_01.jpg'
 
-const ProductsPage = (props) => (
+const ProductsPage = () => (
     <div className="products-page-container">
         <div className="title">PRODUCTS</div>
         <div className="subtitle">DEAR COCOのベストセラー商品</div>
 
         <div className="cards">
-            <div className="tempo-card1" />
-            <div className="tempo-card2" />
-            <div className="tempo-card3" />
-            {/* <ProductsItemCard
+            <ProductsItemCard
                 cardHeading="ボディ&フェイスソープ"
-                image=""
+                image={productsImage1}
                 textbox="全身お使いいただける固形石鹸です。ココナッツオイルの力でしっとり優しく洗い上げます。"
             />
             <ProductsItemCard
                 cardHeading="スキンケアセット"
-                image=""
+                image={productsImage2}
                 textbox="クレンジング、美容液、保湿ローションのセットです。潤いを保ち、肌トラブルを防ぎます。"
             />
             <ProductsItemCard
                 cardHeading="ボディクリーム"
-                image=""
+                image={productsImage3}
                 textbox="お風呂上がりの肌や、乾燥が気になるときに。全身にお使いいただけます。"
-            /> */}
+            />
         </div>
         <div className="tempo-button-container">
-            {/* <AnimatedButton>商品一覧へ</AnimatedButton> */}
-            <div className="tempo-button" />
+            <AnimatedButton url="https://en.wikipedia.org/wiki/Coconut">
+                商品一覧へ
+            </AnimatedButton>
         </div>
     </div>
 );
