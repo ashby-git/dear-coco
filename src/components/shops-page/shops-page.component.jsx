2e2ceb2ecf26249b0ca08fa3cb6ff1ebe4623fbb
Ashby McFall <ashbymcfall@hotmail.co.uk>
2021-10-26 11:51:39 +0900

    💄add&position std button, titles spacing&font

--- src/components/shops-page/shops-page.component.jsx
+++ src/components/shops-page/shops-page.component.jsx
@@ -2,26 +2,23 @@ import React from 'react';
 
 import './shops-page.styles.scss';
 
-// import StandardButton from
+import StandardButton from '../standard-button/standard-button.component'
 
 const ShopsPage = () => (
-    <div>
 
-        <div className="shops-background">
-            <div className="title">SHOPS</div>
-            <div className="subheading">店舗またはオンラインショップよりご購入いただけます</div>
-            <div className="button-group">
-                <div className="temp-button" />
-                <div className="temp-button" />
-                {/* <div className="button-container">
-                    <StandardButton>取扱店舗一覧へ</StandardButton>
-                </div>
-                <div className="button-container"></div>
-                    <StandardButton>公式オンラインショップへ</StandardButton>
-                </div> */}
+    <div className="shops-background">
+        <div className="shops-title">SHOPS</div>
+        <div className="shops-subheading">店舗またはオンラインショップよりご購入いただけます</div>
+        <div className="shops-button-group">
+            <div className="shops-button-container">
+                <StandardButton width="408px" height="96px" url="https://en.wikipedia.org/wiki/Coconut">
+                    取扱店舗一覧へ
+                </StandardButton>
             </div>
+            <StandardButton width="408px" height="96px" url="https://en.wikipedia.org/wiki/Coconut">
+                公式オンラインショップへ
+            </StandardButton>
         </div>
-
     </div>
 );
 
