b6c901aa39d026b33d3814bbe601fd47f2e20e29
Ashby McFall <ashbymcfall@hotmail.co.uk>
2021-10-26 12:41:01 +0900

    ♻️differentiate button styling classnames

--- src/components/standard-button/standard-button.component.jsx
+++ src/components/standard-button/standard-button.component.jsx
@@ -4,8 +4,8 @@ import './standard-button.styles.scss';
 
 const StandardButton = (props) => (
 
-    <a href={props.url} style={{ height: props.height, width: props.width }} className="wrapper">
-        <div className="cta">
+    <a href={props.url} style={{ height: props.height, width: props.width }} className="std-wrapper">
+        <div className="std-cta">
             <p>{props.children}</p>
         </div>
     </a>
