aa4ab23c735b92d6be61fd69a3a5eea005c65d73
Ashby McFall <ashbymcfall@hotmail.co.uk>
2021-10-26 12:35:47 +0900

    💄position elements

--- src/components/products-page/products-page.component.jsx
+++ src/components/products-page/products-page.component.jsx
@@ -11,27 +11,31 @@ import productsImage3 from '../../assets/products_img_01.jpg'
 
 const ProductsPage = () => (
     <div className="products-page-container">
-        <div className="title">PRODUCTS</div>
-        <div className="subtitle">DEAR COCOのベストセラー商品</div>
+        <div className="pp-title">PRODUCTS</div>
+        <div className="pp-subheading">DEAR COCOのベストセラー商品</div>
 
-        <div className="cards">
-            <ProductsItemCard
-                cardHeading="ボディ&フェイスソープ"
-                image={productsImage1}
-                textbox="全身お使いいただける固形石鹸です。ココナッツオイルの力でしっとり優しく洗い上げます。"
-            />
-            <ProductsItemCard
-                cardHeading="スキンケアセット"
-                image={productsImage2}
-                textbox="クレンジング、美容液、保湿ローションのセットです。潤いを保ち、肌トラブルを防ぎます。"
-            />
+        <div className="pp-cards-container">
+            <div className="pp-products-item-card">
+                <ProductsItemCard
+                    cardHeading="ボディ&フェイスソープ"
+                    image={productsImage1}
+                    textbox="全身お使いいただける固形石鹸です。ココナッツオイルの力でしっとり優しく洗い上げます。"
+                />
+            </div>
+            <div className="pp-products-item-card">
+                <ProductsItemCard
+                    cardHeading="スキンケアセット"
+                    image={productsImage2}
+                    textbox="クレンジング、美容液、保湿ローションのセットです。潤いを保ち、肌トラブルを防ぎます。"
+                />
+            </div>
             <ProductsItemCard
                 cardHeading="ボディクリーム"
                 image={productsImage3}
                 textbox="お風呂上がりの肌や、乾燥が気になるときに。全身にお使いいただけます。"
             />
         </div>
-        <div className="tempo-button-container">
+        <div className="pp-button-container">
             <AnimatedButton url="https://en.wikipedia.org/wiki/Coconut">
                 商品一覧へ
             </AnimatedButton>
