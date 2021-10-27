a76f7a5195e99d5fe2b21a4ab00167baeb85bca9
Ashby McFall <ashbymcfall@hotmail.co.uk>
2021-10-27 12:29:02 +0900

    📱improve responsiveness, change tags

--- src/components/products-page/products-page.component.jsx
+++ src/components/products-page/products-page.component.jsx
@@ -14,35 +14,29 @@ const ProductsPage = () => (
     <div className="products-page-container">
 
         <img className="pp-bg-leaf" src={productsLeaf} alt="bg-leaf" />
-        <div className="pp-title">PRODUCTS</div>
-        <div className="pp-subheading">DEAR COCOのベストセラー商品</div>
+        <h1 className="pp-title">PRODUCTS</h1>
+        <h2 className="pp-subheading">DEAR COCOのベストセラー商品</h2>
 
         <div className="pp-cards-container">
-            <div className="pp-products-item-card">
-                <ProductsItemCard
-                    cardHeading="ボディ&フェイスソープ"
-                    image={productsImage1}
-                    textbox="全身お使いいただける固形石鹸です。ココナッツオイルの力でしっとり優しく洗い上げます。"
-                />
-            </div>
-            <div className="pp-products-item-card">
-                <ProductsItemCard
-                    cardHeading="スキンケアセット"
-                    image={productsImage2}
-                    textbox="クレンジング、美容液、保湿ローションのセットです。潤いを保ち、肌トラブルを防ぎます。"
-                />
-            </div>
+            <ProductsItemCard
+                cardHeading="ボディ&フェイスソープ"
+                image={productsImage1}
+                textbox="全身お使いいただける固形石鹸です。ココナッツオイルの力でしっとり優しく洗い上げます。"
+            />
+            <ProductsItemCard
+                cardHeading="スキンケアセット"
+                image={productsImage2}
+                textbox="クレンジング、美容液、保湿ローションのセットです。潤いを保ち、肌トラブルを防ぎます。"
+            />
             <ProductsItemCard
                 cardHeading="ボディクリーム"
                 image={productsImage3}
                 textbox="お風呂上がりの肌や、乾燥が気になるときに。全身にお使いいただけます。"
             />
         </div>
-        <div className="pp-button-container">
-            <AnimatedButton url="https://en.wikipedia.org/wiki/Coconut">
-                商品一覧へ
-            </AnimatedButton>
-        </div>
+        <AnimatedButton url="https://en.wikipedia.org/wiki/Coconut">
+            商品一覧へ
+        </AnimatedButton>
     </div>
 );
 
