484739c2a218db9e106c0dbc23220223584240dc
Ashby McFall <ashbymcfall@hotmail.co.uk>
2021-11-04 12:48:07 +0900

    🐛move bg from app.css to top-page-content styles

--- src/components/all-pages/all-pages.component.jsx
+++ src/components/all-pages/all-pages.component.jsx
@@ -2,8 +2,25 @@ import React from 'react';
 
 import './all-pages.styles.scss';
 
+import Navbar from '../nav-bar/nav-bar.component';
+import TopPageContent from '../top-page/top-page-content.component';
+import ConceptPage from '../concept-page/concept-page.component';
+import QualityPage from '../quality-page/quality-page.component';
+import ProductsPage from '../products-page/products-page.component';
+import ShopsPage from '../shops-page/shops-page.component';
+// import NewsPage from '../news-page/news-page.component';
+// import Footer from '../footer/footer.component';
+
 const AllPages = () => (
     <div>
+        <Navbar />
+        <TopPageContent />
+        <ConceptPage />
+        <QualityPage />
+        <ProductsPage />
+        <ShopsPage />
+        {/* <NewsPage /> */}
+        {/* <Footer /> */}
     </div>
 );
 
