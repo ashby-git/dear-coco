3c0fb941f83c5f7b60ac6b58db3fb8143af1f2a4
Ashby McFall <ashbymcfall@hotmail.co.uk>
2021-11-12 11:13:11 +0900

    ✨make navbar have bg on scroll

--- src/components/nav-bar/nav-bar.component.jsx
+++ src/components/nav-bar/nav-bar.component.jsx
@@ -1,86 +1,101 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-scroll';
 import NavBarItem from './nav-bar-item/nav-bar-item.component.jsx';
 import OnlineShopButton from './online-shop-button/online-shop-button.component.jsx';
 import logo from '../../assets/logo.svg';
 import './nav-bar.styles.scss';
 
-const NavBar = () => (
-    <div className="fixed-position">
-        <div className="nav-bar-container">
+const NavBar = () => {
 
-            <div className="nbi-wrap">
-                <Link
-                    className="nav-bar-item-container"
-                    to="concept"
-                    smooth={true}
-                    duration={500}
-                >
-                    <NavBarItem>Concept</NavBarItem>
-                </Link>
-                <Link
-                    className="nav-bar-item-container"
-                    to="quality"
-                    smooth={true}
-                    duration={500}
-                >
-                    <NavBarItem>Quality</NavBarItem>
-                </Link>
-                <Link
-                    className="nav-bar-item-container"
-                    to="products"
-                    smooth={true}
-                    duration={500}
-                    offset={-180}
-                >
-                    <NavBarItem>Products</NavBarItem>
-                </Link>
-            </div>
+    const [navbarFixed, setNavbarFixed] = useState(false)
 
-            <Link
-                className="logo-container"
-                to="top"
-                smooth={true}
-                duration={500}
-            >
-                <img src={logo} alt="white logo" />
-            </Link>
+    const changeBackground = () => {
+        if (window.scrollY >= 80) {
+            setNavbarFixed(true);
+        } else {
+            setNavbarFixed(false);
+        }
+    };
 
-            <div className="nbi-wrap">
-                <Link
-                    className="nav-bar-item-container"
-                    to="shops"
-                    smooth={true}
-                    duration={500}
-                    offset={-180}
-                >
-                    <NavBarItem>Shops</NavBarItem>
-                </Link>
-                <Link
-                    className="nav-bar-item-container"
-                    to="news"
-                    smooth={true}
-                    duration={500}
-                    offset={35}
-                >
-                    <NavBarItem>News</NavBarItem>
-                </Link>
-                <Link
-                    className="nav-bar-item-container"
-                    to="contact"
-                    smooth={true}
-                    duration={500}
-                    offset={10}
-                >
-                    <NavBarItem>Contact</NavBarItem>
-                </Link>
-            </div>
+    window.addEventListener('scroll', changeBackground)
 
-            <div className="shop-button-container">
-                <OnlineShopButton />
+    return (
+        <div className={navbarFixed ? 'navbarFixed active' : 'navbarFixed'}>
+            <div className="nav-bar-container">
+
+                <div className="nbi-wrap">
+                    <Link
+                        className="nav-bar-item-container"
+                        to="concept"
+                        smooth={true}
+                        duration={500}
+                    >
+                        <NavBarItem>Concept</NavBarItem>
+                    </Link>
+                    <Link
+                        className="nav-bar-item-container"
+                        to="quality"
+                        smooth={true}
+                        duration={500}
+                    >
+                        <NavBarItem>Quality</NavBarItem>
+                    </Link>
+                    <Link
+                        className="nav-bar-item-container"
+                        to="products"
+                        smooth={true}
+                        duration={500}
+                        offset={-180}
+                    >
+                        <NavBarItem>Products</NavBarItem>
+                    </Link>
+                </div>
+
+                <Link
+                    className="logo-container"
+                    to="top"
+                    smooth={true}
+                    duration={500}
+                >
+                    <img src={logo} alt="white logo" />
+                </Link>
+
+                <div className="nbi-wrap">
+                    <Link
+                        className="nav-bar-item-container"
+                        to="shops"
+                        smooth={true}
+                        duration={500}
+                        offset={-180}
+                    >
+                        <NavBarItem>Shops</NavBarItem>
+                    </Link>
+                    <Link
+                        className="nav-bar-item-container"
+                        to="news"
+                        smooth={true}
+                        duration={500}
+                        offset={35}
+                    >
+                        <NavBarItem>News</NavBarItem>
+                    </Link>
+                    <Link
+                        className="nav-bar-item-container"
+                        to="contact"
+                        smooth={true}
+                        duration={500}
+                        offset={10}
+                    >
+                        <NavBarItem>Contact</NavBarItem>
+                    </Link>
+                </div>
+
+                <div className="shop-button-container">
+                    <OnlineShopButton />
+                </div>
             </div>
         </div>
-    </div>
-);
+    )
+};
 
 export default NavBar;
